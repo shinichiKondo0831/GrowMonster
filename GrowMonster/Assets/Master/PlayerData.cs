d8795df106f115b35c79ba4a75eaaeee16befc05
TSITCL\s162078 <forwork199708@gmail.com>
2017-10-15 07:58:28 +0900

    新規プロジェクトをプッシュ

---
+++ GrowMonster/Assets/Master/PlayerData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShinHachi
+{
+    [CreateAssetMenu(menuName = "PlayerData/Create PlayerDataAsset Instance")]
+    public class PlayerData : ScriptableObject
+    {
+
+        [SerializeField]
+        List<PlayerDataList> _list = new List<PlayerDataList>();
+
+        //public PlayerDataList GetData(int id)
+        //{
+        //    if (_list.Count < id || id >= 0)
+        //    {
+        //        return _list[id];
+        //    }
+
+        //    return null;
+        //}
+
+        public int GetLength()
+        {
+            return _list.Count;
+        }
+
+        [System.SerializableAttribute]
+        public class PlayerDataList
+        {
+            public int Hp;
+            public int AttackPowerBias;
+            public float MoveSpeed;
+        }
+    }
+}
---
+++ GrowMonster/Assets/Scripts/Player/PlayerStatus.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShinHachi
+{
+    public class PlayerStatus {
+
+        /// <summary>
+        /// プレイヤー内部データ
+        /// </summary>
+        public class Data
+        {
+            /// <summary>
+            /// コンストラクタ
+            /// </summary>
+            /// <param name="velocity">スピード</param>
+            /// <param name="power">攻撃力</param>
+            /// <param name="health">HP</param>
+            public Data(float velocity, int power, int health, int maxHealth)
+            {
+                this._health = health;
+                this._power = power;
+                this._velocity = velocity;
+                this._maxHealth = maxHealth;
+            }
+
+            //-------プロパティ内部変数-----------------//
+            // プレイヤーの速度
+            public float _velocity = 0.0f;
+
+            // プレイヤーの力
+            public int _power = 0;
+
+            // プレイヤーのHp
+            public int _health = 0;
+
+            // プレイヤーの最大Hp
+            public int _maxHealth = 0;
+
+            
+        }
+
+        //------------------------------------------//
+        //-------プロパティ設定---------------------//
+        //------------------------------------------//
+        //-------使用例-----------------------------//
+        //------------------------------------------//
+        //-------変数名 = 変数名.プロパティ名-------//
+        //------------------------------------------//
+
+        Data _data = new Data(0f, 0, 0, 0);
+        //------*ゲッター&セッター*------//
+
+        /// <summary>
+        /// プレイヤーのデータを返す
+        /// </summary>
+        public Data PublicPlayerData
+        {
+            get { return _data; }
+        }
+
+
+        /// <summary>
+        /// スピード
+        /// </summary>
+        public float Velocity
+        {
+            get { return _data._velocity; }
+            set
+            {
+                _data._velocity = value;
+            }
+        }
+
+        /// <summary>
+        /// HP
+        /// </summary>
+        public int Health
+        {
+            get { return _data._health; }
+            set
+            {
+                _data._health = value;
+            }
+        }
+
+        /// <summary>
+        /// 攻撃力
+        /// </summary>
+        public int Power
+        {
+            get { return _data._power; }
+            set
+            {
+                _data._power = value;
+            }
+        }
+
+        /// <summary>
+        /// 最大HP
+        /// </summary>
+        public int MaxHealth
+        {
+            get { return _data._maxHealth; }
+            set
+            {
+                _data._maxHealth = value;
+            }
+        }
+
+    }
+}
+
