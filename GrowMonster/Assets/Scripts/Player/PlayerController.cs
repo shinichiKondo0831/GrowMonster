15630d0cd57e9eaf8c5f8767548855257456b138
TSITCL\s162078 <forwork199708@gmail.com>
2017-12-18 14:23:47 +0900

    PlayerInfo Add Player movement control complete

--- GrowMonster/Assets/Scripts/Player/PlayerController.cs
+++ GrowMonster/Assets/Scripts/Player/PlayerController.cs
@@ -2,44 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// PlayerInfoのスクリプトを一緒にアタッチできるようにする
+[RequireComponent(typeof(PlayerInfo))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    public string csvName_ = null; 
-    // csv読み込み行ったデータ格納コンテナ
-    private List<string[]> csvData_ = new List<string[]>();
-    // プレイヤーの内部データ
     ShinHachi.PlayerStatus data_ = new ShinHachi.PlayerStatus();
 
-    enum STATUS
+    // Use this for initialization
+    void Start ()
     {
-        HELTH,
-        SPEED,
-        POWER
-    };
-
-	// Use this for initialization
-	void Start () {
-        StartCoroutine("CharacterData");
-        Debug.Log("内部データ");
-        Debug.Log(data_.Health);
-        Debug.Log(data_.Velocity);
-        Debug.Log(data_.Power);
-    }
-
-    /// <summary>
-    /// Characterのデータを読み取るまで次の処理を行わない
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerator CharacterData()
-    {
-        CSVReader csv_ = new CSVReader();
-        csv_.CsvRead(csvName_);
-        data_.Health = int.Parse(csv_.OutputData()[0][(int)STATUS.HELTH]);
-        data_.Velocity = int.Parse(csv_.OutputData()[0][(int)STATUS.SPEED]);
-        data_.Power = int.Parse(csv_.OutputData()[0][(int)STATUS.POWER]);
-        yield return data_;
-        
+        data_ = GetComponent<PlayerInfo>().Data;
     }
   
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * data_.Speed * Time.deltaTime;
+        }
+
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= transform.right * data_.Speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * data_.Speed * Time.deltaTime;
+        }
+
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.forward * data_.Speed * Time.deltaTime;
+        }
+
+    }
 }
---
+++ GrowMonster/Assets/Scripts/Player/PlayerInfo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+
+    [SerializeField]
+    public string csvName_ = null;
+    // csv読み込み行ったデータ格納コンテナ
+    private List<string[]> csvData_ = new List<string[]>();
+    // プレイヤーの内部データ
+    ShinHachi.PlayerStatus data_ = new ShinHachi.PlayerStatus();
+
+    public ShinHachi.PlayerStatus Data
+    {
+        get { return data_;}
+    }
+
+    enum STATUS
+    {
+        HELTH,
+        SPEED,
+        POWER
+    };
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(CharacterData());
+    }
+
+    /// <summary>
+    /// Characterのデータを読み取るまで次の処理を行わない
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator CharacterData()
+    {
+        CSVReader csv_ = new CSVReader();
+        csv_.CsvRead(csvName_);
+        data_.Health = int.Parse(csv_.OutputData()[0][(int)STATUS.HELTH]);
+        data_.Speed = int.Parse(csv_.OutputData()[0][(int)STATUS.SPEED]);
+        data_.Power = int.Parse(csv_.OutputData()[0][(int)STATUS.POWER]);
+
+        yield break;
+    }
+
+    void Update()
+    {
+
+    }
+}
